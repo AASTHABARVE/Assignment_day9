98d3b5739eeacb1b2aeddf8292a770e501517bc1
aastha <aasthabarve0@gmail.com>
2023-03-21 11:18:07 +0530

    calculate daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,4 +2,25 @@
 
 
 
+
+
 echo " Welcome to Employee Wage Computation Program"
+
+
+
+ispresent=1;
+randomcheck="$((RANDOM%2))";
+
+if [ $ispresent -eq $randomcheck ];
+then
+echo "Employee is present";
+
+else
+
+echo "Empoloyee is not present";
+
+fi
+
+
+
+
