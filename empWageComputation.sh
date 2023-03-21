3d7f46574f772dcdcf25b342b1f4eccb2ed2969f
aastha <aasthabarve0@gmail.com>
2023-03-21 11:41:41 +0530

    UC 3 add part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,6 +8,7 @@ echo " Welcome to Employee Wage Computation Program"
 
 
 
+
 ispresent=1;
 randomcheck="$((RANDOM%2))";
 
@@ -22,5 +23,21 @@ echo "Empoloyee is not present";
 fi
 
 
+ispresentt=1;
+randomcheckk="$((RANDOM%2))";
+
+if [ $ispresentt -eq $randomcheckk ];
+then
+       empratehr=20;
+        emphr=8;
+salary=$(($empratehr*$emphr));
+
+echo $salary;
+
+else
+
+salary=0;
+
+fi
 
 
